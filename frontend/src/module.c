00b60fbffc34da4d66c90780bfa5aeead28d81f9
cohnon <me@cohnon.com>
2025-10-19 09:22:22 -0700

    parse directly to section buffers

--- frontend/src/module.c
+++ frontend/src/module.c
@@ -1,196 +1,37 @@
 #include "module.h"
-#include <stdlib.h>
 
-void print_type(FILE *out, Type *type) {
-    switch (type->kind) {
-    case TYPE_UNIT:
-        fprintf(out, "unit");
-        break;
+#include "cosm.h"
 
-    case TYPE_I8:
-        fprintf(out, "i8");
-        break;
-
-    case TYPE_I32:
-        fprintf(out, "i32");
-        break;
-
-    case TYPE_WORD:
-        fprintf(out, "word");
-        break;
-
-    case TYPE_ARRAY:
-        fprintf(out, "[%ld x ", type->arr.len);
-        print_type(out, type->arr.ty);
-        fprintf(out, "]");
-        break;
-
-    case TYPE_PTR:
-        fprintf(out, "*");
-        print_type(out, type->ptr);
-        break;
-
-    case TYPE_FUNC:
-        fprintf(out, "fn ");
-        print_type(out, type->func.in);
-        fprintf(out, " -> ");
-        print_type(out, type->func.out);
-        break;
-
-    case TYPE_TUP:
-        fprintf(out, "(");
-        for (int i = 0; i < type->fields.len; i++) {
-            print_type(out, type->fields.items[i]);
-
-            if (i + 1 < type->fields.len) {
-                fprintf(out, ", ");
-            }
-        }
-        fprintf(out, ")");
-        break;
-
-    default:
-        fprintf(out, "<unhandled type>");
-        break;
-    }
+void write_i32(int32_t n, FILE *out) {
+    fwrite(&n, sizeof(n), 1, out);
 }
 
-long parse_number(Token tok)
-{
-    long x = 0;
-    for (size_t i = 0; i < tok.len; i++) {
-        x = x * 10 + (tok.src[i] - '0');
-    }
-
-    return x;
+void write_i64(int64_t n, FILE *out) {
+    fwrite(&n, sizeof(n), 1, out);
 }
 
-void print_value(FILE *out, Value *val) {
-    switch (val->type) {
-    case VAL_SYMBOL:
-    case VAL_VARIABLE:
-        fprintf(out, "%.*s", TOK_FMT(val->tok));
-        break;
-
-    case VAL_NUMBER:
-        fprintf(out, "%ld", parse_number(val->tok));
-        break;
-
-    case VAL_TUPLE:
-        fprintf(out, "(");
-        for (int i = 0; i < val->tup.len; i++) {
-            print_value(out, val->tup.items[i]);
-            if (i + 1 < val->tup.len) {
-                fprintf(out, ", ");
-            }
-        }
-        fprintf(out, ")");
-        break;
-
-    case VAL_REF:
-        fprintf(out, "ref ");
-        print_value(out, val->val);
-        break;
-
-    default:
-        fprintf(out, "<unhandled value>");
-        break;
-    }
-}
-
-void print_block(FILE *out, Block *blk) {
-    for (int i = 0; i < blk->instrs.len; i++) {
-        fprintf(out, "    ");
-
-        Instr *instr = blk->instrs.items[i];
-        if (instr->var != NULL) {
-            print_value(out, instr->var);
-            fprintf(out, " = ");
-        }
-
-        switch (instr->type) {
-        case INSTR_ADD:
-            fprintf(out, "add ");
-            print_value(out, instr->val);
-            break;
-
-        case INSTR_IMM:
-            fprintf(out, "imm ");
-            print_value(out, instr->val);
-            break;
-
-        case INSTR_CALL:
-            fprintf(out, "call %.*s ", TOK_FMT(instr->call.target));
-            print_value(out, instr->call.args);
-            break;
-
-        case INSTR_RET:
-            fprintf(out, "ret");
-            if (instr->val != NULL) {
-                fprintf(out, " ");
-                print_value(out, instr->val);
-            }
-            break;
-
-        default:
-            fprintf(out, "<unhandled instr>");
-            break;
-        }
-
-        fprintf(out, "\n");
-    }
-}
-
-void print_function(FILE *out, Item *item) {
-    for (int i = 0; i < item->func.blks.len; i++) {
-        Block *blk = item->func.blks.items[i];
-        if (i != 0) {
-            fprintf(out, "blk.%d\n", i);
-        }
-        print_block(out, blk);
-    }
-}
-
-void print_item(FILE *out, Item *item) {
-    fprintf(out, "%.*s ", (int)item->sym_len, item->sym);
-
-    switch (item->vis) {
-    case ITEM_VIS_GLOBAL:
-        fprintf(out, "global ");
-        break;
-
-    case ITEM_VIS_FOREIGN:
-        fprintf(out, "foreign ");
-        break;
-
-    case ITEM_VIS_LOCAL:
-        break;
-    }
-
-    print_type(out, item->ty);
-
-    if (item->vis == ITEM_VIS_FOREIGN) {
-        fprintf(out, "\n");
+void write_section(COSM_SecType type, ByteArray buf, FILE *out) {
+    if (buf.len == 0) {
         return;
     }
 
-    if (item->ty->kind == TYPE_FUNC) {
-        fprintf(out, "\n");
-        print_function(out, item);
-        fprintf(out, "end\n");
-    } else {
-        fprintf(out, " ");
-        print_value(out, item->obj);
-        fprintf(out, "\n");
-    }
+    write_i32(type, out);
+    write_i64(buf.len, out);
+    fwrite(buf.items, buf.len, 1, out);
 }
 
-void print_module(FILE *out, Module *mod) {
-    for (int i = 0; i < mod->items.len; i++) {
-        Item *item = mod->items.items[i];
-        print_item(out, item);
-        if (i + 1 < mod->items.len) {
-            fprintf(out, "\n");
-        }
-    }
+void write_header(FILE *out) {
+    fwrite(COSM_MAGIC, sizeof(COSM_MAGIC), 1, out);
+    write_i32(COSM_VERSION, out);
+}
+
+void write_module(Module *mod, FILE *out) {
+    write_header(out);
+    write_section(COSM_SEC_IMPORTS, mod->imports, out);
+    write_section(COSM_SEC_EXPORTS, mod->exports, out);
+    write_section(COSM_SEC_STRINGS, mod->strings, out);
+    write_section(COSM_SEC_TYPES,   mod->types,   out);
+    write_section(COSM_SEC_ITEMS,   mod->items,   out);
+    write_section(COSM_SEC_CODE,    mod->code,    out);
+    write_section(COSM_SEC_DATA,    mod->data,    out);
 }
--- shared/cosm.h
+++ shared/cosm.h
@@ -4,13 +4,50 @@
 #include <stdint.h>
 
 #define COSM_MAGIC ("cosm")
+#define COSM_VERSION (0)
 
-typedef uint64_t CosmWord;
-typedef uint32_t CosmIdx;
+typedef uint64_t COSM_Word;
+typedef uint32_t COSM_Idx;
 
-enum CosmSecType {
-    COSM_SEC_CUSTOM,
-};
-typedef uint32_t CosmSecType;
+typedef enum COSM_SecType {
+    COSM_SEC_CUSTOM = 0x00,
+    COSM_SEC_IMPORTS,
+    COSM_SEC_EXPORTS,
+    COSM_SEC_STRINGS,
+    COSM_SEC_TYPES,
+    COSM_SEC_ITEMS,
+    COSM_SEC_CODE,
+    COSM_SEC_DATA,
+} COSM_SecType;
+
+typedef enum COSM_Type {
+    COSM_TYPE_UNIT = 0x00,
+
+    COSM_TYPE_I8,
+    COSM_TYPE_I16,
+    COSM_TYPE_I32,
+    COSM_TYPE_I64,
+
+    COSM_TYPE_WORD,
+    COSM_TYPE_HALF,
+
+    COSM_TYPE_F32,
+    COSM_TYPE_F64,
+
+    COSM_TYPE_PTR,
+    COSM_TYPE_ARRAY,
+    COSM_TYPE_SLICE,
+    COSM_TYPE_FUNC,
+
+    COSM_TYPE_TUP,
+    COSM_TYPE_UNION,
+} CosmType;
+
+typedef enum COSM_InstrType {
+    COSM_INSTR_ADD,
+    COSM_INSTR_IMM,
+    COSM_INSTR_CALL,
+    COSM_INSTR_RET,
+} COSM_InstrType;
 
 #endif
